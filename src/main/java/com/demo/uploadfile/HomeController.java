3263b09cec59d51bae4be3bdf6489693d6900414
lisy <sunytonyli@gmail.com>
2018-06-12 15:27:46 +0800

    add ajax upload file

---
+++ src/main/java/com/demo/uploadfile/HomeController.java
@@ -0,0 +1,85 @@
+package com.demo.uploadfile;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Handles requests for the application home page. 
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	/**
+	 * Upload single file using Spring Controller
+	 */
+	@RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+	@ResponseBody
+	public String uploadFileHandler(@RequestParam("name") String name,
+			@RequestParam("fileName") MultipartFile file) {
+
+		if (!file.isEmpty()) {
+			try {
+				byte[] bytes = file.getBytes();
+				name = file.getOriginalFilename();
+				
+				// Creating the directory to store file
+				String rootPath = System.getProperty("catalina.home");
+				File dir = new File(rootPath + File.separator + "tmpFiles");
+				if (!dir.exists())
+					dir.mkdirs();
+
+				// Create the file on server
+				File serverFile = new File(dir.getAbsolutePath() + File.separator + name);
+				BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
+				stream.write(bytes);
+				stream.close();
+
+				logger.info("Server File Location=" + serverFile.getAbsolutePath());
+
+				return "You successfully uploaded file=" + name;
+				
+			} catch (Exception e) {
+				
+				return "You failed to upload " + name + " => " + e.getMessage();
+				
+			}
+		} else {
+			
+			return "You failed to upload " + name+ " because the file was empty.";
+			
+		}
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,47 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<h1>
+	Hello world!  
+</h1>
+
+<P>  The time on the server is ${serverTime}. </P>
+<form action="">
+
+	<input id="default_file" type="file" name="fileName"/>
+
+</form>
+
+<script type="text/javascript" src="resources/js/jquery-1.11.0.min.js"></script>
+<script type="text/javascript">
+	$('#default_file').change(function(){    
+	    //on change event  
+	    var formdata = new FormData();
+	    if($(this).prop('files').length > 0)
+	    {
+	        file =$(this).prop('files')[0];
+	        formdata.append("fileName", file);
+	        formdata.append("name", "test");
+	    }
+	    
+	    $.ajax({
+		    url: "uploadFile",
+		    type: "POST",
+		    data: formdata,
+		    processData: false,
+		    contentType: false,
+		    success: function (result) {
+		         // if all is well
+		         // play the audio file
+		         alert(result);
+		    }
+		});
+	});
+	
+</script>
+</body>
+</html>
